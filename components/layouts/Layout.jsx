375b1a00a2e6326d974b2f413749a4b62ce72f0f
Ngoc NT <tanngoc93@gmail.com>
2019-05-29 08:00:05 +0000

    Updated components/layouts/Layout.jsx

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -9,20 +9,33 @@ function Layout(props) {
   const img = (props.image || '')
   const url = `${config.site.domainName}${props.asPath || ''}`
   const title = `${entitiesDecode(props.title)} - ${config.site.name}`
-  const description = props.excerpt ? entitiesDecode(props.excerpt) : config.site.description
-
-  console.log('props.asPath', props.asPath)
+  const desc = props.excerpt ? entitiesDecode(props.excerpt) : config.site.description
 
   return (
     <div>
       <Head>
+        {/* HTML Meta Tags */}
         <title>{ title }</title>
-        <meta name="description" content={ description }></meta>
-        <meta property="og:description" content={ description } ></meta>
-        <meta property="og:title" content={ title } ></meta>
-        <meta property="og:type" content="article" ></meta>
-        <meta property="og:image" content={ img } ></meta>
-        <meta property="og:url" content={ url } ></meta>
+        <meta name="description" content={ desc } />
+
+        {/* Google / Search Engine Tags */}
+        <meta itemprop="description" content={ desc } />
+        <meta itemprop="name" content={ title } />
+        <meta itemprop="image" content={ img } />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:type" content="article" />
+        <meta property="og:description" content={ desc } />
+        <meta property="og:title" content={ title } />
+        <meta property="og:image" content={ img } />
+        <meta property="og:url" content={ url } />
+
+        {/* Twitter Meta Tags */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:description" content={ desc } />
+        <meta property="twitter:title" content={ title } />
+        <meta property="twitter:image" content={ img } />
+        <meta property="twitter:url" content={ url } />
       </Head>
 
       { props.body }
