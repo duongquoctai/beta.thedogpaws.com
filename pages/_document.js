cedd6c9f3c9f22581e5eaab0fbfb68664fba7d01
Ngoc NT <tanngoc93@gmail.com>
2019-05-29 08:15:22 +0000

    Updated react

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,28 @@
 import config from "../services/publicConfig"
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import { Fragment } from "react"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const isProduction = process.env.NODE_ENV === "production"
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    return { ...initialProps, isProduction }
+  }
+
+  setGoogleTags() {
+    return {
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-120985317-1');
+      `
+    }
   }
 
   render() {
+    const { isProduction } = this.props
+
     return (
       <Html>
         <Head>
@@ -18,6 +33,14 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          {isProduction && (
+            <Fragment>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120985317-1" />
+              <script dangerouslySetInnerHTML={ this.setGoogleTags() } />
+            </Fragment>
+          )}
+
           <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" />
           <script defer src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" />
           <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" />
